7d291e90ed0231f41ee6d29e1dcc0f147b327855
odrevet <odrevet@gmail.com>
2026-01-05 21:23:55 +0100

    default room and coords

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ from game import Game
 def main() -> None:
     # Initialize argument parser
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="LandStalker")
-    parser.add_argument('-r', '--room', type=int, default=1, help='Room number')
+    parser.add_argument('-r', '--room', type=int, default=148, help='Room number')
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug mode')
-    parser.add_argument('-x', type=float, default=0.0, help='Hero starting X position')
-    parser.add_argument('-y', type=float, default=0.0, help='Hero starting Y position')
-    parser.add_argument('-z', type=float, default=0.0, help='Hero starting Z position')
+    parser.add_argument('-x', type=float, default=20, help='Hero starting X position')
+    parser.add_argument('-y', type=float, default=25, help='Hero starting Y position')
+    parser.add_argument('-z', type=float, default=8, help='Hero starting Z position')
     parser.add_argument('-f', '--fullscreen', action='store_true', help='Starts fullscreen')
     parser.add_argument('-W', '--width', type=int, default=320, help='Window width (default: 320)')
     parser.add_argument('-H', '--height', type=int, default=224, help='Window height (default: 224)')
